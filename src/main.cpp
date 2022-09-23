546d56ee898bb6b912c80b6007d90da6cf01a42b
Lance Riedel <lanceriedel@gmail.com>
2022-09-23 13:27:23 -0500

    added laser detector

--- src/main.cpp
+++ src/main.cpp
@@ -1,169 +1,176 @@
+/*
+ Basic MQTT example
+
+ This sketch demonstrates the basic capabilities of the library.
+ It connects to an MQTT server then:
+  - publishes "hello world" to the topic "outTopic"
+  - subscribes to the topic "inTopic", printing out any messages
+    it receives. NB - it assumes the received payloads are strings not binary
+
+ It will reconnect to the server if the connection is lost using a blocking
+ reconnect function. See the 'mqtt_reconnect_nonblocking' example for how to
+ achieve the same result without blocking the main loop.
+ 
+*/
 
 #include <SPI.h>
-#include <Wire.h>
-
-
-//#include <SPI.h>
-// Ethernet - Version: Latest 
 #include <Ethernet.h>
-
-// Enter a MAC address and IP address for your controller below.
-// The IP address will be dependent on your local network.
-// gateway and subnet are optional:
-byte mac[] = {
-  0x00, 0xAA, 0xBB, 0xCC, 0xDE, 0x02
-};
-IPAddress ip(192, 168, 1, 177);
-IPAddress myDns(192, 168, 1, 1);
-IPAddress gateway(192, 168, 1, 1);
-IPAddress subnet(255, 255, 255, 0);
-IPAddress mqttserver(192, 168, 1, 5);
+#include <PubSubClient.h>
+#include <Wire.h>
+#include "Adafruit_TCS34725.h"
 
 
 
-char SerialBuf[80];
-String strFromSerial; // saving 1 line string
-String strFromMobileAPP;
-String strLocalIP;
+//for laser detecting
+Adafruit_TCS34725 tcs = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_24MS, TCS34725_GAIN_16X);
+int led = 13;
+int avg_values_countdown = 20;
+uint32_t total_k = 0;
+uint32_t total_r = 0;
+uint32_t avg_k = 0;
+uint32_t avg_r=0;
+const int32_t MAX_TEMP_DIFF=600;
 
-#include <MQTT.h>
-EthernetClient net;
-MQTTClient client;
+// Update these with values suitable for your network.
+byte mac[]    = {  0xDE, 0xED, 0xBA, 0xFE, 0xFE, 0xED };
+IPAddress ip(192,168,1,5);
+IPAddress server(192, 168, 1, 112);
 
-// it's a public name space, so open the key & secret value.
-#define MQTT_DeviceName "AVR_NODE_1"
-
-#define MQTT_topic_Message  "/Message"
-
-
-
-void connect() {
-  Serial.print("connecting...");
-
-  while (!client.connect(MQTT_DeviceName)) {
-    Serial.print(".");
-    delay(1000);
-  }
-
-  Serial.println("\nconnected!");
-
-  client.subscribe(MQTT_topic_Message);
-  // client.unsubscribe(MQTT_topic_Message);
-}
-
-void MQTT_control_run()
-{
-  client.loop();
-
-  if (!client.connected()) {
-    connect();
-  }
-
-  // publish a message roughly every second.
-  /*
-  if (millis() - lastMillis > 10000) {
-    lastMillis = millis();
-    client.publish("/hello", "world");
-  }*/
-
-}
-
-
-
-void messageReceived(String &topic, String &payload) {
-  Serial.println("incoming: " + topic + " - " + payload);
-  if (topic==MQTT_topic_Message){
-    strFromMobileAPP = payload;
-  }
-}
-
-void printText() {
-  if (strFromMobileAPP.length())
-  {
-        Serial.print(strFromMobileAPP);
-
-  }
-}
-
-
-void User_application_run()
-{
-  printText();
-}
-
-
-char* ip2CharArray(IPAddress ip) {
-  static char a[16];
-  sprintf(a, "%d.%d.%d.%d", ip[0], ip[1], ip[2], ip[3]);
-  return a;
-}
-
-
-void setup()   {                
-  // You can use Ethernet.init(pin) to configure the CS pin
-  Ethernet.init(10);  // Most Arduino shields
-  //Ethernet.init(5);   // MKR ETH shield
-  //Ethernet.init(0);   // Teensy 2.0
-  //Ethernet.init(20);  // Teensy++ 2.0
-  //Ethernet.init(5);  // ESP8266 with Adafruit Featherwing Ethernet
-  //Ethernet.init(33);  // ESP32 with Adafruit Featherwing Ethernet
-
-  // Open serial communications and wait for port to open:
-  Serial.begin(9600);
-      while (!Serial) {
-          ; // wait for serial port to connect. Needed for native USB port only
-      }
-
-  // start the Ethernet connection:
-  Serial.println("Trying to get an IP address using DHCP");
-  if (Ethernet.begin(mac) == 0) {
-    Serial.println("Failed to configure Ethernet using DHCP");
-    // Check for Ethernet hardware present
-    if (Ethernet.hardwareStatus() == EthernetNoHardware) {
-      Serial.println("Ethernet shield was not found.  Sorry, can't run without hardware. :(");
-      while (true) {
-        delay(1); // do nothing, no point running without Ethernet hardware
-      }
-    }
-    if (Ethernet.linkStatus() == LinkOFF) {
-      Serial.println("Ethernet cable is not connected.");
-    }
-    // initialize the Ethernet device not using DHCP:
-    Ethernet.begin(mac, ip, myDns, gateway, subnet);
-  }
-
-  Ethernet.MACAddress(mac); // fill the MAC buffer
-  Serial.print("The MAC address is: ");
-  for (byte octet = 0; octet < 6; octet++) {
-    Serial.print(mac[octet], HEX);
-    if (octet < 5) {
-      Serial.print('-');
-    }
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (int i=0;i<length;i++) {
+    Serial.print((char)payload[i]);
   }
   Serial.println();
-
-  // print your local IP address:
-  ip = Ethernet.localIP(); // update the IP address buffer
-  Serial.print("My IP address: ");
-  Serial.println(Ethernet.localIP());
-
-
- 
-  delay(2000); // Pause for 2 seconds
-
-  // Clear the buffer
-  strLocalIP = "IP: " + String(ip2CharArray(Ethernet.localIP()));
-  strFromMobileAPP = strLocalIP;
-
-  // set the MQTT broker address to MQTTClient
-  client.begin(mqttserver, 1883, net);
-  // set the callback function for receiving
-  client.onMessage(messageReceived);
-  // connnect to MQTT broker
-  connect();
 }
 
-void loop() {
-  MQTT_control_run();
-  User_application_run();
+EthernetClient ethClient;
+PubSubClient client(ethClient);
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Attempt to connect
+    if (client.connect("arduinoClient")) {
+      Serial.println("connected");
+      // Once connected, publish an announcement...
+      client.publish("outTopic","hello world");
+      // ... and resubscribe
+      client.subscribe("inTopic");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+
+void laser_init() {
+  Serial.println(" ");
+  while (avg_values_countdown) {
+    uint16_t r, g, b, c, colorTemp;
+    int32_t lux;
+    tcs.getRawData(&r, &g, &b, &c);
+    // colorTemp = tcs.calculateColorTemperature(r, g, b);
+    colorTemp = tcs.calculateColorTemperature_dn40(r, g, b, c);
+    lux = tcs.calculateLux(r, g, b);
+
+    Serial.print("Color Temp: "); Serial.print(colorTemp, DEC); Serial.print(" K - ");
+    Serial.print("C: "); Serial.print(c, DEC); Serial.print(" - ");
+    Serial.print("LUx: "); Serial.print(lux); Serial.print(" - ");
+    Serial.print("R: "); Serial.print(r, DEC); Serial.print(" -");
+    Serial.print("G: "); Serial.print(g, DEC); Serial.print(" -");
+    Serial.print("B: "); Serial.print(b, DEC); Serial.println(" -");
+
+  
+    total_k+=colorTemp;
+    total_r+=r;
+    delay(500);
+    Serial.println("Calibrating... "); Serial.println(" ");
+    avg_values_countdown--;
+  }
+
+  //Calculate the Average
+  if (avg_k==0) {
+    avg_k = total_k/20;
+    avg_r = total_r/20;
+    Serial.print("AVG K: "); Serial.print(avg_k, DEC); Serial.println(" ");
+    Serial.print("AVG R: "); Serial.print(avg_r, DEC); Serial.println(" ");
+
+    digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
+    delay(4000);               // wait for a second
+    digitalWrite(led, LOW); 
+  }
+
+
+}
+
+void laser_trigger() {
+    client.publish("avr-building","shot-fired".);
+
+    Serial.print("FLASH LED"); Serial.println(" ");
+
+    digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
+    delay(1000);               // wait for a second
+    digitalWrite(led, LOW); 
+
+}
+void laser_detect() {
+  uint16_t r, g, b, c, colorTemp;
+  int32_t lux;
+  
+
+  tcs.getRawData(&r, &g, &b, &c);
+  // colorTemp = tcs.calculateColorTemperature(r, g, b);
+  colorTemp = tcs.calculateColorTemperature_dn40(r, g, b, c);
+  lux = tcs.calculateLux(r, g, b);
+
+  Serial.print("Color Temp: "); Serial.print(colorTemp, DEC); Serial.print(" K - ");
+  Serial.print("C: "); Serial.print(c, DEC); Serial.print(" - ");
+  Serial.print("LUx: "); Serial.print(lux); Serial.print(" - ");
+  Serial.print("R: "); Serial.print(r, DEC); Serial.print(" -");
+  Serial.print("G: "); Serial.print(g, DEC); Serial.print(" -");
+  Serial.print("B: "); Serial.print(b, DEC); Serial.print(" -");
+
+
+  int32_t dff_temp = avg_k-colorTemp;
+  if (dff_temp<0) dff_temp = dff_temp * -1;
+  if (dff_temp>100000) dff_temp = 0;
+  //if ((abs(avg_r-r))>100) {
+    Serial.print("DIFF K: "); Serial.print(dff_temp); Serial.println(" ");
+
+  if (dff_temp>MAX_TEMP_DIFF) {
+    laser_trigger();
+  }
+
+}
+
+void setup()
+{
+  Serial.begin(9600);
+
+  client.setServer(server, 1883);
+  client.setCallback(callback);
+
+  Ethernet.begin(mac, ip);
+  // Allow the hardware to sort itself out
+  delay(1500);
+  laser_init();
+
+
+}
+
+void loop()
+{
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+  laser_detect();
+
 }
