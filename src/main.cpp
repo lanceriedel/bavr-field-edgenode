ca7cd4d12eff18327ec8c635d0200b056b568797
Lance Riedel <lanceriedel@gmail.com>
2022-09-26 10:48:32 -0500

    add back initialization

--- src/main.cpp
+++ src/main.cpp
@@ -51,6 +51,36 @@ void callback(char* topic, byte* payload, unsigned int length) {
   controller.callback(topic, payload,length);
 }
 
+void ethernet_setup() {
+   if (Ethernet.begin(mac) == 0) {
+
+    Serial.println("Failed to configure Ethernet using DHCP");
+
+    // Check for Ethernet hardware present
+
+    if (Ethernet.hardwareStatus() == EthernetNoHardware) {
+      Serial.println("Ethernet shield was not found.  Sorry, can't run without hardware. :(");
+      while (true) {
+        delay(1); // do nothing, no point running without Ethernet hardware
+      }
+    }
+
+    if (Ethernet.linkStatus() == LinkOFF) {
+      Serial.println("Ethernet cable is not connected.");
+    }
+
+    // initialize the Ethernet device not using DHCP:
+    //Ethernet.begin(mac, ip);
+    Serial.println("Could not obtain IP Address.");
+    exit(1);
+  }
+
+  // print your local IP address:
+  Serial.print("My IP address: ");
+  Serial.println(Ethernet.localIP());
+
+}
+
 void setup()
 {
   Serial.begin(9600);
@@ -64,9 +94,14 @@ void setup()
   outputBuffer[i] = 0;
   unique_id = String(outputBuffer);
   Serial.print("BOARD_ID:"); Serial.println(unique_id);
-  Ethernet.begin(mac, ip);
+  // Ethernet setup
+  ethernet_setup();
   // Allow the hardware to sort itself out
   delay(1500);
+
+  //pubsub init
+  client.setServer(server, 1883);
+  client.setCallback(callback);
 
   //leds
   led_animations.setup();
