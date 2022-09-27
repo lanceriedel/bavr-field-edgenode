1fa537c31d2aa030c64b60dcc42e5a38ee02e6e2
Sam Barbee <sambarbee@me.com>
2022-09-27 01:25:20 -0500

    Fixed Ref in Main

--- src/main.cpp
+++ src/main.cpp
@@ -12,6 +12,7 @@ NOTE -  no delays should be anywhere in the loops -- will work to make sure that
 #include <PubSubClient.h>
 #include <Wire.h>
 
+#include "TroughDetect.hpp"
 #include "LaserDetect.hpp"
 #include "LEDAnimations.hpp"
 #include "BAVRFieldComms.hpp"
@@ -29,6 +30,8 @@ BAVRFieldComms field_comms;
 LaserDetect laser_detect;
 // for leds
 LEDAnimations led_animations;
+
+TroughDetect trough_detect;
 
 // Update these with values suitable for your network.
 byte mac[]    = {  0xDE, 0xED, 0xBA, 0xFE, 0xFE, 0xED };
@@ -116,7 +119,7 @@ void setup()
 
   Serial.println(F("Setup Done begin loops..."));
 
-  controller = new BAVRFieldController(&led_animations, &laser_detect,  &field_comms);
+  controller = new BAVRFieldController(&led_animations, &laser_detect,  &field_comms, &trough_detect);
   field_comms.setup("TBD", &client);
   delay(1500);
 
