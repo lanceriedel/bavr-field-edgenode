a7a625d120b8f5b74246ff71f10f70650dc26ab9
Lance Riedel <lanceriedel@gmail.com>
2022-10-31 17:45:42 -0500

    removed wrong commit of subscribe initialize

--- src/BAVRFieldComms.cpp
+++ src/BAVRFieldComms.cpp
@@ -22,12 +22,8 @@ void BAVRFieldComms::reconnect() {
     // Attempt to connect
 
     if (client->connect(unique_id)) {
-      char topic[256];
-      memset(topic, 0, sizeof(topic));
-      strcpy(topic, "nodered/initialization/");
-      strcat(topic, unique_id);
       Serial.println("connected");
-      client->subscribe(topic);
+      client->subscribe("nodered/initialization/#");
       delay(1000);
 
     } else {
